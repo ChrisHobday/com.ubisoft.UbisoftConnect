0bf91c3d6e96e5abe232fdeb70f4832f8854ff2b
Chris Hobday <hobday.chris@protonmail.com>
2025-07-02 13:47:37 -0400

    Apply modifications every launch to ensure updates are applied even when already installed

--- Scripts/UbisoftConnect.sh
+++ Scripts/UbisoftConnect.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
+echo "Running Wineboot"
+wineboot
+
+echo "Setting Windows version to 10"
+winecfg -v win10
+
+# echo "Updating registry"
+# wine regedit /app/share/WineGraphics.reg
+# sleep 3
+
+echo "Setting up Discord rich presence"
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i ||
+    ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
 echo "Checking if Ubisoft Connect is already installed"
-if [ -d "/var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher" ]; then
+if [ -d "/var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/" ]; then
   echo "Ubisoft Connect is installed"
 
-  echo "Setting up Discord rich presence"
-  for i in {0..9}; do
-      test -S $XDG_RUNTIME_DIR/discord-ipc-$i ||
-      ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
-  done
+  echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/"
+  cd "/var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/"
 
-  echo "Changing directory to /var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher"
-  cd /var/data/wine/drive_c/Program\ Files\ \(x86\)/Ubisoft/Ubisoft\ Game\ Launcher/
-
-  # TODO: Find out why and fix Ubisoft Connect failing to launch after installed
   echo "Launching Ubisoft Connect"
-  wine /var/data/wine/drive_c/Program\ Files\ \(x86\)/Ubisoft/Ubisoft\ Game\ Launcher/UbisoftConnect.exe
+  wine "/var/data/wine/drive_c/Program Files (x86)/Ubisoft/Ubisoft Game Launcher/UbisoftConnect.exe"
 else
   echo "Ubisoft Connect is not installed"
 
-  echo "Initializing Wine prefix"
-  wineboot -i
-
-  echo "Launching Battle.netSetup.exe"
+  echo "Launching Ubisoft Connect Installer"
   wine /app/bin/UbisoftConnectInstaller.exe
 fi
